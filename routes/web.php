f74076bd49ff05bf57418e72d54fef06edd69ff2
Aleandro2119 <aleandro.basilico@gmail.com>
2022-03-22 18:56:02 +0100

    add printed the first project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = 'Il mio primo progetto in Laravel';
+    return view('home', ['title' => $title]);
 });
